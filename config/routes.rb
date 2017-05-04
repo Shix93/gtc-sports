c838814903943ec5034bb57cb271dd2d5b7500fd
Petar <petarsijan993@gmail.com>
2017-05-04 13:02:05 +0000

    Added controller, routes and views for leagues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'leagues#index'
+
+  match ':controller(/:action(/:id))', :via => [:get, :post, :put]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
